52c613c25c6cf8d22c4ed3fab5dbef44660c7197
SmallDevil <a0923865232@gmail.com>
2022-07-13 16:25:54 +0800

    Update

--- National/52th_project/Module_A/16/06.php
+++ National/52th_project/Module_A/16/06.php
@@ -4,12 +4,6 @@ header("content-type:image/jpeg");
 $img = imagecreate(400,100);
 imagecolorallocate($img,255,255,255);
 $str = array_keys($_GET)[0];
-// for ($j=0; $j < 2; $j++) { 
-//     $str .= chr(rand(65,90));
-// }
-// for ($j=0; $j < 3; $j++) { 
-//     $str .= rand(0,9);
-// }
 foreach (str_split($str) as $key => $value) {
     imagettftext($img,80,rand(-20,20),40+$key*80,rand(80,90),imagecolorallocate($img,rand(0,100),rand(0,100),rand(0,100)),'arial',$value);
 }
